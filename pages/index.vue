4f9d5679565ed68477259b5c9b4d314f2e41bd23
samuelOsborne <samu.osborne@gmail.com>
2020-06-24 13:12:23 +0200

    Init project with scss, bootstrap-vue

---
+++ pages/index.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="container app-font-family">
+    howdy
+    <div class="row">
+      <div class="col">
+        lolol
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  scripts: {
+
+  }
+}
+</script>
+
+<style>
+.title {
+  font-family:
+    'Quicksand',
+    'Source Sans Pro',
+    -apple-system,
+    BlinkMacSystemFont,
+    'Segoe UI',
+    Roboto,
+    'Helvetica Neue',
+    Arial,
+    sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+</style>
